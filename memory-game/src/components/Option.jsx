e1736a326e6df96d989a1f91f25bb54c08e873e9
kairlytauk <kairly.tauk@outlook.com>
2025-06-17 14:53:53 -0400

    Initial commit

--- memory-game/src/components/Option.jsx
+++
@@ -1,9 +0,0 @@
-export default function Option({ valueArray }) {
-    const optionEl = valueArray.map(({ name, value }) => (
-        <option key={value} value={value}>
-            {name ? name : value}
-        </option>
-    ))
-    
-    return <>{optionEl}</>
-}
