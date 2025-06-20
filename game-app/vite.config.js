4e8416ad95f5fbcec26c4e7757047c3c73ae4be3
kairlytauk <kairly.tauk@outlook.com>
2025-06-20 02:00:22 -0400

    commit 4

--- game-app/vite.config.js
+++ game-app/vite.config.js
@@ -1,8 +1,18 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [
-		react()
-	]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    // Add this to support Vercel's routing fallback
+    fs: { allow: ['.'] }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
